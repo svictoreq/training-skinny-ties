c8d40f1c4dc161b4638922cc1de884f0c63429f7
Victor Requena <svictoreq@gmail.com>
2018-03-23 11:21:58 -0400

    Add resize slider toggle functionality

--- js/app.js
+++ js/app.js
@@ -1,27 +1,48 @@
 $(document).ready(function() {
-	$('.owl-carousel').owlCarousel({
-		loopg: false,
-		margin: 0,
-		nav: true,
-		navContainer: '.headline',
-		navText: ['<img src="img/icons/prev.svg" />', '<img src="img/icons/next.svg" />'],
-		dots: false,
-		responsive: {
-			0: {
-				items: 3
-			},
-			480: {
-				items: 7
-			},
-			1024: {
-				items: 9
+	const createCarousel = () => {
+		return $('.owl-carousel').owlCarousel({
+			loopg: false,
+			margin: 0,
+			nav: true,
+			navContainer: '.owl-controls',
+			navText: ['<img src="img/icons/prev.svg" />', '<img src="img/icons/next.svg" />'],
+			dots: false,
+			responsive: {
+				0: {
+					items: 3
+				},
+				480: {
+					items: 4
+				},
+				768: {
+					items: 7
+				},
+				1024: {
+					items: 9
+				}
 			}
+		});
+	};
+
+	if ($(window).width() <= 1150) {
+		createCarousel();
+	} else {
+		$('.owl-carousel').addClass('no-carousel');
+	}
+
+	$(window).resize(function() {
+		if ($(window).width() <= 1150) {
+			$('.owl-carousel').removeClass('no-carousel');
+			createCarousel();
+		} else {
+			$('.owl-carousel').trigger('destroy.owl.carousel')
+			$('.owl-carousel').addClass('no-carousel');
 		}
-	});
+	})
 
 	let slideBttnTimeout;
 
-	$('button[class*="owl"]').on('click', function() {
+	$(document).on('click', 'button[class*="owl"]', function() {
 		if ($(this).hasClass('owl-next')) {
 			$('button.owl-prev').removeClass('clicked');
 		} else {
